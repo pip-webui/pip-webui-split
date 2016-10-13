fc81baca8f758d82e603c3bc057ac45752a62306
Anastas Fonotov <anastasf@gmail.com>
2016-10-13 15:35:52 +0300

    update split

--- dist/pip-webui-split.js
+++ dist/pip-webui-split.js
@@ -32,14 +32,16 @@
             function(event, toState, toParams, fromState, fromParams) {
                 // Split animation
                 var splitElements = $('.pip-split');
+                
                 if (splitElements.length > 0) {
                     splitElements.removeClass('pip-transition-forward');
                     splitElements.removeClass('pip-transition-back');
                     if (toState.name != fromState.name) {
-                        if (pipSplit.forwardTransition(toState, fromState))
+                        if (pipSplit.forwardTransition(toState, fromState)) {
                             splitElements.addClass('pip-transition-forward');
-                        else
+                        } else {
                             splitElements.addClass('pip-transition-back');
+                        }
                     }
                 }
 
@@ -64,20 +66,25 @@
         //----------------------------
 
         function addTransitionSequence(sequence) {
-            if (!_.isArray(sequence) || sequence.length == 0)
+            if (!_.isArray(sequence) || sequence.length == 0) {
                 throw new Error('Transition sequence must be an array of state names');
+            }
 
             transitionSequences.push(sequence);
         }
 
         function forwardTransition(toState, fromState) {
-            for (var i = 0; i < transitionSequences.length; i++) {
-                var toIndex = transitionSequences[i].indexOf(toState.name);
-                var fromIndex = transitionSequences[i].indexOf(fromState.name);
+            var i, toIndex, fromIndex;
 
-                if (toIndex > -1)
+            for (i = 0; i < transitionSequences.length; i++) {
+                toIndex = transitionSequences[i].indexOf(toState.name),
+                fromIndex = transitionSequences[i].indexOf(fromState.name);
+
+                if (toIndex > -1) {
                     return toIndex > fromIndex;
+                }
             }
+
             return false
         }
 
